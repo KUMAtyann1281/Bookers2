ee2ad56810ff9f554a54cd889d118f9c1335b5d9
EC2 Default User <ec2-user@ip-172-31-46-129.ap-northeast-1.compute.internal>
2024-08-29 11:05:02 +0000

    [Add] テストによるエラー確認とバリデーションの追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
+  
+  validates :title,
+    length: { minimum: 1, maximum: 50 }
+
+  validates :body,
+    length: { minimum: 1, maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
+  validates :name,
+    length: { minimum: 2, maximum: 20 }
+
+  validates :introduction,
+    length: { minimum: 0, maximum: 50 }
+    
+  validates :email, uniqueness: true
+
   has_one_attached :profile_image
 
   private
